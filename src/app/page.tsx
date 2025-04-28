7c8ab3d5f44571d1b6ee855923e0adc7c5eac507
Oyetech3 <oyetech30@gmail.com>
2025-04-28 07:27:03 +0100

    up

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 
 export default function Home() {
+  
   return (
     <div className="items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <h1 className="text-4xl font-bold">Welcome to the project</h1>
